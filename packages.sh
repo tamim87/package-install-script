bed4c2ea40e2a3c0cda627ecea5200183655dc36
tamim87 <noormuhammadtamim@gmail.com>
2023-12-11 00:07:22 +0600

    script start

---
+++ packages.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+sudo apt install gedit
+aptin curl
+aptin gcc -y
+aptin g++ -y
+aptin neovim -y
+aptin git -y
+aptin neofetch -y
+aptin libfuse2 -y
+aptin exa -y
+aptin vlc -y
+aptin mpv -y
+aptin guake -y
+aptin gnome-shell-extensions -y
+aptin gnome-shell-extension-manager -y
+aptin qpdfview -y
+aptin zathura -y
+aptin libreoffice -y
+aptin gnome-tweaks -y
+aptin geany -y
+aptin geany-plugins -y
+aptin solaar -y
+
+
+
+# starship
+curl -sS https://starship.rs/install.sh | sh
+sudo snap install spotify 
+sudo snap install bitwarden 
+
+# jetbrains mono font
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+# sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update && sudo apt install sublime-text
+
+# cloudflare warp
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ jammy main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+aptup && aptin cloudflare-warp -y
+
+warp-cli register
+warp-cli connect
+warp-cli set-mode warp+doh
+
+
+
+# onlyoffice
+mkdir -p -m 700 ~/.gnupg
+gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+chmod 644 /tmp/onlyoffice.gpg
+sudo chown root:root /tmp/onlyoffice.gpg
+sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
+
+echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+
+aptup && aptin onlyoffice-desktopeditors
+
+
+# dracula theme for gnome-terminal
+sudo apt install dconf-cli
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+./install.sh
+
+
+# dracula theme for gedit
+wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml
+mkdir /home/tamim/.local/share/gedit/styles/
+mv dracula.xml $HOME/.local/share/gedit/styles/
+# Activate in Gedit's preferences dialog
+
+
+
+
+
+
